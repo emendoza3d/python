84dd5d6919d6b4890c810fbea984f046c84658a6
ernesto <emendoza324@students.mchenry.edu>
2026-08-31 11:38:49 -0500

    Add RPG character script with variable definitions and print statements

---
+++ RPGcharactor.py
@@ -0,0 +1,23 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS (DO NOT DELETE)
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Define a String variable.
+[ ] 3. Define an Integer variable.
+[ ] 4. Define a Float variable.
+[ ] 5. Define a Boolean variable.
+[ ] 6. Print all variables using F-Strings.
+[ ] 7. Upload to GitHub.
+-----------------------------------------------------------------------
+"""
+
+charactor_name = "Jimmy"
+hit_points = 500
+strength = 50.25
+alive = True
+
+print(f"Hero: {charactor_name}")
+print(f"Hp: {hit_points}")
+print(f"str: {strength}")
+print(f"alive: {alive}")
