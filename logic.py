9d7fa746068fd0e5a9b35a511302882391734e04
ernesto <emendoza324@students.mchenry.edu>
2026-09-02 12:51:46 -0500

    Add initial implementation of logical checks and user input for two integers

---
+++ logic.py
@@ -0,0 +1,19 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included with assignment title.
+[ ] 2. Ask user for two integers (num1 and num2).
+[ ] 3. Perform 6 logical checks: (Both > 0, Both > 100, Either Even, Either < 100, Not Equal, Not Zero).
+[ ] 4. Use if/elif/else to categorize num1 (Positive/Negative/Zero).
+[ ] 5. Code is clean and uses descriptive variable names.
+[ ] 6. Upload to GitHub and paste the link below.
+-----------------------------------------------------------------------
+"""
+num1 = int(input("number 1:  "))
+num2 = int(input("number 2:  "))
+
+if num1 > 0 & num2 >0:
+    print("They are both greater than 0")
+if num1 = 2 & num2 = 2:
+    print("then the sum is 4")
